5e5ba496b1cefde57aeceeac99e274ef8be5c5c3
Richard Márquez <marquez@ula.ve>
2015-04-22 23:51:12 -0430

    Including local file

--- bibtexbrowser-bs.local.php
+++ bibtexbrowser-bs.local.php
@@ -2,7 +2,4 @@
 // or any valid query: year=2010, author=Jane, etc.
 @define('BIBTEXBROWSER_DEFAULT_FRAME','all');
 @define('BIBTEXBROWSER_BIB_IN_NEW_WINDOW',true);
-
-// include('IEEEStyleForBibtexbrowser.php');
-// define('BIBLIOGRAPHYSTYLE','IEEEStyle');
 ?>
